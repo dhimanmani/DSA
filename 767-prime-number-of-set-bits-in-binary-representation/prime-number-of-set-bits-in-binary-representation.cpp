046139a8856b28b23270e56dc4d7470d5ef6ef2a
Mani Dhiman <manidhiman2005@gmail.com>
2026-03-20 13:52:44 +0530

    Time: 3 ms (67.71%) | Memory: 8 MB (40.32%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    bool prime(int n){
-        if(n<=1) return false;
-        for(int i=2; i*i<=n; i++){
-            if(n%i==0) return false;
-        }
-        return true;
-    }
-    string dectobin(int n){
-        int len= (int)(log2(n));
-        return bitset<64>(n).to_string().substr(64-len-1);
-    }
-    bool countset(int n){
-        string bin= dectobin(n);
-        int c=0;
-        for(int i=0; i<bin.size(); i++){
-            if(bin[i]=='1'){
-                c++;
+    int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            int setBits = __builtin_popcount(i);
+            if (isPrime(setBits)) {
+                count++;
             }
         }
-        bool ans=prime(c);
-        return ans;
-    }
-    int countPrimeSetBits(int left, int right) {
-        int count=0;
-        for(int i=left; i<=right; i++){
-            if(countset(i)) count++;
-        }
         return count;
+    }
+private:
+    bool isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
