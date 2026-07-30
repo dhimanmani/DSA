c0f811f387bf3c8f63cd826b18f4edf7f9b391d5
Mani Dhiman <manidhiman2005@gmail.com>
2026-07-30 15:32:32 +0530

    Time: 4 ms (42.29%) | Memory: 34.7 MB (13.60%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
         unordered_map<int, int>freq;
-        for(int i: bulbs){
-            freq[i]++;
-        }
-        int n=bulbs.size();
+        int n= bulbs.size();
         vector<int>res;
+        for(int i=0; i<n; i++){
+            freq[bulbs[i]]++;
+        }
         for(auto &it: freq){
-            if(it.second%2!=0){
+            if(it.second % 2!=0){
                 res.push_back(it.first);
             }
         }
